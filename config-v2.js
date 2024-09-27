7fa86fdb320b0388f4f58b77b4f2743c9b85c672
Nethaking <darcknetha@gmail.com>
2024-09-27 15:31:58 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~yNc22CoL#r0IJsZQYvkhzgne8x7c38Hb2d-_l1BdDVi8J2q63HgI"
 
 
 module.exports = {
